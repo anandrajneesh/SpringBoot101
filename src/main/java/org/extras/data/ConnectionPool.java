6d4156e16cde50d5eddbb95623e6c51195fcd334
Anand Rajneesh <anand_rajneesh@infosys.com>
2017-03-03 16:33:00 +0530

    word doc handling

--- src/main/java/org/extras/data/ConnectionPool.java
+++ src/main/java/org/extras/data/ConnectionPool.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
-import java.util.Properties;
 import java.util.TreeSet;
 import java.util.concurrent.Semaphore;
 
