604f714fa85649fea17f77284f38748a346a6b4a
Anand Rajneesh <anand_rajneesh@infosys.com>
2017-03-08 19:30:17 +0530

    word doc handling

--- src/main/java/org/extras/config/Configuration.java
+++ src/main/java/org/extras/config/Configuration.java
@@ -1,39 +1,104 @@
 package org.extras.config;
 
-import org.extras.data.Access;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mvel2.MVEL;
+import org.mvel2.templates.CompiledTemplate;
+import org.mvel2.templates.TemplateCompiler;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
-import java.sql.*;
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by Anand_Rajneesh on 3/3/2017.
  */
+@Component
+@ConfigurationProperties
 public class Configuration {
 
-   /* public static void main(String[] args) {
-        Connection conn = null;
-        try{
-            conn= DriverManager.getConnection("jdbc:ucanaccess://D:/extraz/Database11.accdb");
-            Statement s = conn.createStatement();
-            ResultSet rs = s.executeQuery("select * from [Contacts] where company='foobar'");
-            while(rs.next()){
-                System.out.println(rs.getString(5));
-            }
+    private String tarrifRules;
+    private String excludeRules;
+    private String notAcceptedDoc;
+    private String acceptedDoc;
+    private String outputDir;
+    private List<String> dateParameters;
 
+    private Serializable compiledTarrifRules;
+    private Serializable compiledExcludeRules;
 
+    @PostConstruct
+    public void initTemplates(){
+        CompiledTemplate template = TemplateCompiler.compileTemplate(new File(tarrifRules));
+        compiledTarrifRules = MVEL.compileExpression(template.getTemplate());
 
-        }catch(Exception e){
-            e.printStackTrace();
-            try {
-                if(conn!=null) {
-                    conn.close();
-                }
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        }
+        template = TemplateCompiler.compileTemplate(new File(excludeRules));
+        compiledExcludeRules = MVEL.compileExpression(template.getTemplate());
+
     }
-*/
 
+    public String getTarrifRules() {
+        return tarrifRules;
+    }
 
+    public void setTarrifRules(String tarrifRules) {
+        this.tarrifRules = tarrifRules;
+    }
+
+    public String getExcludeRules() {
+        return excludeRules;
+    }
+
+    public void setExcludeRules(String excludeRules) {
+        this.excludeRules = excludeRules;
+    }
+
+    public String getNotAcceptedDoc() {
+        return notAcceptedDoc;
+    }
+
+    public void setNotAcceptedDoc(String notAcceptedDoc) {
+        this.notAcceptedDoc = notAcceptedDoc;
+    }
+
+    public String getAcceptedDoc() {
+        return acceptedDoc;
+    }
+
+    public void setAcceptedDoc(String acceptedDoc) {
+        this.acceptedDoc = acceptedDoc;
+    }
+
+    public String getOutputDir() {
+        return outputDir;
+    }
+
+    public void setOutputDir(String outputDir) {
+        this.outputDir = outputDir;
+    }
+
+    public List<String> getDateParameters() {
+        return dateParameters;
+    }
+
+    public void setDateParameters(List<String> dateParameters) {
+        this.dateParameters = dateParameters;
+    }
+
+    public Serializable getCompiledExcludeRules() {
+        return compiledExcludeRules;
+    }
+
+    public void setCompiledExcludeRules(Serializable compiledExcludeRules) {
+        this.compiledExcludeRules = compiledExcludeRules;
+    }
+
+    public Serializable getCompiledTarrifRules() {
+        return compiledTarrifRules;
+    }
+
+    public void setCompiledTarrifRules(Serializable compiledTarrifRules) {
+        this.compiledTarrifRules = compiledTarrifRules;
+    }
 }
--- src/main/java/org/extras/rest/EmployeeService.java
+++ src/main/java/org/extras/rest/EmployeeService.java
@@ -1,42 +1,92 @@
 package org.extras.rest;
 
-import org.extras.data.Access;
-import org.extras.data.Word;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.extras.DocPdfUtil;
+import org.extras.MvelRunner;
+import org.extras.config.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Collections;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by Anand_Rajneesh on 2/20/2017.
  */
 @Controller
-@RequestMapping("/employee")
+@RequestMapping("/insurance")
 public class EmployeeService {
 
     @Autowired
-    Access access;
-    @Autowired
-    Word word;
+    private Configuration configuration;
 
-    @GetMapping
+    @Autowired
+    private MvelRunner mvelRunner;
+
+    @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    String home() {
+    String insurance(@RequestBody String request){
+        String pdfPath = null;
         try {
-            Map<String, Object> params = new HashMap<String, Object>();
-            params.put("company", "foobar");
-            params = access.search(params);
-            System.out.println(params);
-            word.write(Collections.<String, Object>emptyMap());
-            return "done";
+            Gson gson = new Gson();
+            JsonObject jsonObject = gson.fromJson(request, JsonObject.class);
+            System.out.println(jsonObject);
+            Map<String, Object> flatMap = flatMapJson(jsonObject);
+            boolean excludeFromTarrif = mvelRunner.executeExcludeRules(flatMap);
+            if(excludeFromTarrif){
+                //TODO not accepted document
+                pdfPath = newFilePath();
+                DocPdfUtil.createPdfFromDoc(configuration.getNotAcceptedDoc(), flatMap, pdfPath);
+            }else{
+                //TODO accepted document calculate tarrif
+                Map<String, Object> tarrifMap = mvelRunner.executeTariffRules(flatMap);
+                flatMap.putAll(tarrifMap);
+                pdfPath = newFilePath();
+                DocPdfUtil.createPdfFromDoc(configuration.getAcceptedDoc(), flatMap, pdfPath);
+            }
         }catch (Exception e){
-            return "error";
+            e.printStackTrace();
+            pdfPath = null;
         }
+        return pdfPath;
+
     }
+
+    private String newFilePath(){
+        return configuration.getOutputDir()+"/"+UUID.randomUUID()+".pdf";
+    }
+
+    private Map<String,Object> flatMapJson(JsonObject jsonObject) throws ParseException {
+        Map<String, Object> flatMapped = new HashMap<String, Object>();
+        for(Map.Entry<String,JsonElement> entry:  jsonObject.entrySet()){
+            JsonElement element = entry.getValue();
+            if(element.isJsonObject()){
+                flatMapped.putAll(flatMapJson(element.getAsJsonObject()));
+            }else if(element.isJsonPrimitive()){
+                if(configuration.getDateParameters().contains(entry.getKey())){
+                    flatMapped.put(entry.getKey()+"_date",getDate(element.getAsString()));
+                }
+                flatMapped.put(entry.getKey(), element.getAsString());
+            }
+        }
+        return flatMapped;
+    }
+
+    private Date getDate(String date) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("mm/dd/yyyy");
+        return formatter.parse(date);
+    }
+
+
 
 }
